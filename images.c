c39ac75e92b5a216f8ed80b26d04346299d4b62b
summer <summer@localhost.localdomain>
2024-04-30 18:05:57 -0700

    main menu loop

--- images.c
+++ images.c
@@ -4,8 +4,15 @@
 #include <stdio.h>
 
 int main () {
-
-
+    int mainchoice;
+    
+    do {
+        printf ("**ERINSTAGRAM**\n1: Load image\n2: Display image\n3: Edit image\n0: Exit\nChoose from one of the options above: ");
+        scanf ("%d", &mainchoice);   
+    
+    } while (mainchoice != 0);
+    
+    printf ("Goodbye!\n");
 return 0;
 }
 
