8ab95caa455871d07a30474e00e0057182ff9a92
summer <summer@localhost.localdomain>
2024-05-02 13:57:06 -0700

    added pointers as parameters and display function

--- images.c
+++ images.c
@@ -5,23 +5,27 @@
 #define ROWS 500
 #define COLS 500
 
-int editmenu(int rows, int cols, int theimagearray[][COLS]);
-int crop(int rows, int cols, int newTrow, int newBrow, int newLcol, int newRcols, int theimagearray[][COLS]);
+void loadImage(int* rowPtr, int* colPtr, int rows, int cols, int theimagearray[][COLS]);
+void displayImage(int rows, int cols, int theimagearray[][COLS], int* rowPtr, int* colPtr);
+int editmenu(int rows, int cols, int theimagearray[][COLS], int* rowPtr, int* colPtr);
+int crop(int rows, int cols, int newTrow, int newBrow, int newLcol, int newRcols, int theimagearray[][COLS], int* rowPtr, int* colPtr);
 void brightenImage(int rows, int cols, int theimagearray[][COLS]);
 void dimImage(int rows, int cols, int theimagearray[][COLS]);
 
 int main () {
-    int theimagearray[ROWS][COLS], mainchoice;
+    int theimagearray[ROWS][COLS], mainchoice, rowsize, colsize;
     
     do {
         printf ("**ERINSTAGRAM**\n1: Load image\n2: Display image\n3: Edit image\n0: Exit\nChoose from one of the options above: ");
         scanf ("%d", &mainchoice);   
         if (mainchoice == 1) {
+            
         }
         else if (mainchoice == 2) {
+            displayImage(ROWS, COLS, theimagearray, &rowsize, &colsize);
         }
         else if (mainchoice == 3) {
-           editmenu(ROWS, COLS, theimagearray);
+           editmenu(ROWS, COLS, theimagearray, &rowsize, &colsize);
         }
         
     } while (mainchoice != 0);
@@ -29,8 +33,18 @@ int main () {
     printf ("Goodbye!\n");
 return 0;
 }
+void loadImage(int* rowPtr, int* colPtr, int rows, int cols, int theimagearray[][COLS]) {
+
+}
+void displayImage(int rows, int cols, int theimagearray[][COLS], int* rowPtr, int* colPtr) {
+    for (int i=0; i < *rowPtr; i++) {
+        for (int j=0; j < *colPtr; j++) {
+            printf ("%d", theimagearray[i][j]);
+        }
+    }
+}
 //Won't compile because the pointers for the actual image row size and column size (called rowsize and colsize) haven't been declared or assigned values yet. Need to use these pointers in the load function with fscanf for the sizes
-int editmenu (int rows, int cols, int theimagearray[][COLS]) {
+int editmenu (int rows, int cols, int theimagearray[][COLS], int* rowPtr, int* colPtr) {
     int editchoice, cropLcol, cropRcol, cropTrow, cropBrow;
     printf ("**EDITING**\n1: Crop image\n2: Dim image\n3: Brighten image\n0: Return to main menu\nChoose from one of the options above: ");
     scanf ("%d", &editchoice);
@@ -40,7 +54,7 @@ int editmenu (int rows, int cols, int theimagearray[][COLS]) {
        printf ("Which column do you want to be the new right side? ");
        scanf ("%d", &cropRcol);
        if (cropRcol <= cropLcol) {
-           printf ("Invalid column value. Choose a value between %d and %d: ", cropLcol, *colsize);
+           printf ("Invalid column value. Choose a value between %d and %d: ", cropLcol, *colPtr);
            scanf ("%d", &cropRcol);
        }
        printf ("Which row do you want to be the new top? ");
@@ -48,15 +62,23 @@ int editmenu (int rows, int cols, int theimagearray[][COLS]) {
        printf ("Which row do you want to be the new bottom? ");
        scanf ("%d", &cropBrow);
        if (cropBrow <= cropTrow) {
-           printf ("Invalid row value. Choose a value between %d and %d: ", cropTrow, *rowsize);
+           printf ("Invalid row value. Choose a value between %d and %d: ", cropTrow, *rowPtr);
            scanf ("%d", &cropBrow);
        }
-       crop(rows, cols, cropTrow, cropBrow, cropLcol, cropRcol, theimagearray);
+       crop(rows, cols, cropTrow, cropBrow, cropLcol, cropRcol, theimagearray, rowPtr, colPtr);
        
-       return 0;
+    }
+    else if (editchoice == 2) {
+        dimImage(rows, cols, theimagearray);
+    }
+    else if (editchoice == 3) {
+        brightenImage(rows, cols, theimagearray);
+    }
+    else if (editchoice == 0) {
+        return 0;
     }
 }
-int crop(int rows, int cols, int newTrow, int newBrow, int newLcol, int newRcol, int theimagearray[][COLS]) {
+int crop(int rows, int cols, int newTrow, int newBrow, int newLcol, int newRcol, int theimagearray[][COLS], int* rowPtr, int* colPtr) {
     
 }
 
